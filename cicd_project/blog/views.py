1c14ac9fe060866acebedaaa4e8cbc654610bb06
soohyun-lee <soohyun527@gmail.com>
2021-08-09 17:40:12 +0900

    first commit

---
+++ cicd_project/blog/views.py
@@ -0,0 +1,22 @@
+import json
+from django.http import JsonResponse
+from django.views import View
+from .models import Booklist
+
+class Book(View):
+    def get(self, request):
+        all_booklist = Booklist.objects.all()
+
+        data = ['aws', 'test']
+        a = Booklist.objects.filter(name__in=data)
+        if a.count() == len(data):
+            return JsonResponse({'message' : data}, status=200) 
+        else:
+            return JsonResponse({'message' : 'no'}, status=200) 
+
+        data = [{
+            'name' : book.name,
+            'category' : book.category
+        } for book in all_booklist]
+
+        return JsonResponse({'message' : data}, status=200)
