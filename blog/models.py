1f1200b9cfea30eea38ce28ddd6c0ec9be7240c1
soohyun-lee <lydia@adoc.co.kr>
2021-10-06 15:04:32 +0900

    마이그레이션 테스트

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Booklist(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length = 100)
     tag = models.CharField(max_length=100)
+    tag_secodne = models.CharField(max_length=100)
 
     class Meta:
         db_table = 'booklist'
