dc746206aba6d7f552985a5bb15805fc3b324057
soohyun-lee <lydia@adoc.co.kr>
2021-10-06 15:08:15 +0900

    마이그레이션 테스트

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.db.models.fields import CharField, DateTimeField
 
 class Booklist(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length = 100)
-    tag = models.CharField(max_length=100)
-    tag_secodne = models.CharField(max_length=100)
-    second = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length = 100, null=True)
+    tag = models.CharField(max_length=100, null=True)
+    tag_secodne = models.CharField(max_length=10, null=True)
+    second = models.CharField(max_length=100, null=True)
 
     class Meta:
         db_table = 'booklist'
